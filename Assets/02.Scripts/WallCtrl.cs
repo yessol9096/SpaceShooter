61bbc6e9e85f17ed114773085bb2e434b3ff6902
yessol9096 <yessol9096@naver.com>
2018-12-28 17:45:20 +0900

    벽과 총알 충돌시 파티클 생성
    파티클 소멸이 안됨

--- Assets/02.Scripts/WallCtrl.cs
+++ Assets/02.Scripts/WallCtrl.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class WallCtrl : MonoBehaviour {
 
+	public GameObject sparkEffect;
+
+	// 충돌을 시작할 때 발생하는 이벤트
 	void OnCollisionEnter(Collision coll)
 	{
 		if (coll.collider.tag == "BULLET") {
+
+			GameObject spark = (GameObject) Instantiate(sparkEffect, coll.transform.position, Quaternion.identity);
+			// 컴포넌트의 수행시간이 지난 후 삭제 처리
+			Destroy (spark, spark.GetComponent<ParticleSystem>().duration + 0.2f);
+
 			Destroy (coll.gameObject);
 		}
 	}
